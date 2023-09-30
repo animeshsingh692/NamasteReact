7fc60d2770d568143630a17f2133d1b55da92519
Animesh Singh <animeshsingh692@gmail.com>
2023-09-30 18:38:40 +0530

    Configure package.json to add scripts to build and start the app + Introduction to JSX

---
+++ LayingTheFoundation/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello,</h1>
+            <p>from JSX</p>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render()
